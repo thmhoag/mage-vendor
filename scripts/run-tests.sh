50f8491b8fcfb785769ca1fc15d8a9cd78695d1d
Thomas Hoag <thmhoag@gmail.com>
2019-11-15 15:31:20 -0500

    Add test running script

---
+++ scripts/run-tests.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+REPODIR="$(dirname "$SCRIPTDIR")"
+cd "$REPODIR"
+
+set -e
+
+need_luarocks_msg="
+    
+    You must have the luarocks and busted cli tools installed for testing this repository.
+
+    To install using apt-get: apt-get install luarocks
+
+    Additional instructions found here: http://olivinelabs.com/busted/#usage
+
+"
+command -v luarocks >/dev/null 2>&1 || { printf "luarocks is required but not installed...\n$need_luarocks_msg" >&2; exit 1; }
+command -v busted >/dev/null 2>&1 || { printf "busted is required but not installed...\n$need_luarocks_msg" >&2; exit 1; }
+
+busted ./test -p _Tests
